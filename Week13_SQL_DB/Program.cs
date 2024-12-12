b27228884e9eb762761cd3489fae4b9140a784a1
KadriGit <kadri.viira@tktk.ee>
2024-12-12 21:27:10 +0200

    Part1

--- Week13_SQL_DB/Program.cs
+++ Week13_SQL_DB/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.Data.SQLite;
+
+CreateConnection();
+
+static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; version=3;new=True;Compress=True;");
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection established");
+    }
+    catch
+    {
+        Console.WriteLine("DB Connection failed");
+    }
+
+    return connection;
+}
+
